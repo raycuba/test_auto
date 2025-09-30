1a51b882a2a7ac7e98e0b003bb8585cdb6bfff88
raycuba <raycubA15@gmail.com>
2025-09-30 11:44:39 +0200

    configuraciones de Django para el proyecto test_auto.

--- test_auto/settings.py
+++ test_auto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^+1xk8x6z!wx=jt15asj&e*%54!q98aa3sh)z26$usve=+97^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
